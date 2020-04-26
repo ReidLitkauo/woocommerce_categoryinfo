1979d45ba5ecb0e7882d168943a0f64e1ae3394c
Reid Litkauo <reid.litkauo@gmail.com>
2020-04-25 22:01:51 -0700

    First round of polishing
    Now features only three database queries per order placed! Yayyyyyyyy for optimization

--- woocommerce_categoryinfo/woocommerce_categoryinfo.php
+++ woocommerce_categoryinfo/woocommerce_categoryinfo.php
@@ -1,88 +1,111 @@
 <?php
 /**
  * Plugin Name: woocommerce_categoryinfo
- * Description: Add category information to WooCommerce webhook data.
+ * Description: Add category information to line items sent to WooCommerce webhooks.
  * Version: 1.0
  * Author: Reid Litkauo
  * Author URI: https://reid.litkauo.com
  */
 
-function thisthingy($response, $object, $request) {
+// I plan on adding the category information in a series of steps:
+// 1) Compile a list of all product_id's for this order.
+// 2) DB: Find all term_taxonomy_id's for these product_id's
+// 3) DB: Find all term_id's for these term_taxonomy_id's
+// 4) DB: Find all category information for these term_id's
+// 5) Match up all the category information to their appropriate products.
+
+function add_categories($response, $object, $request) {
 	global $wpdb;
 
-error_log(print_r($response, TRUE));
+	// 1) Get all the product_id's for this order.
+	
+	// Just iterate over all the line items
+	// foreach preserves order, so $product_id[$i] == $response->data['line_items'][$i]['product_id']
+	$product_ids = [];
+	foreach($response->data['line_items'] as $lineitem) {
+		$product_ids[] = $lineitem['product_id'];
+	}
+	unset($lineitem);
 
-	foreach( $response->data['line_items'] as $k => $item ) {
+	// Return if no product_id's
+	if (!$product_ids)
+		return $response;
 
-		$term_taxonomy_ids = $wpdb->get_col( $wpdb->prepare(
-			"SELECT term_taxonomy_id FROM {$wpdb->prefix}term_relationships WHERE object_id = %d",
-			$item['product_id'] ) );
-error_log(print_r('term taxonomy ids', TRUE));
-error_log(print_r($term_taxonomy_ids, TRUE));
+	// 2) DB: Find all term_taxonomy_id's for these product_id's
 
-		$categories = array();
+	// Let's hit the database
+	$map_pid2ttid = $wpdb->get_results( $wpdb->prepare(
+		"SELECT object_id,term_taxonomy_id
+			FROM {$wpdb->prefix}term_relationships
+			WHERE object_id IN (" . implode(',', $product_ids) . ")"
+		 ), ARRAY_A );
 
-		foreach( $term_taxonomy_ids as $ttid ) {
-error_log(print_r('going thru this ttid', TRUE));
-error_log(print_r($ttid, TRUE));
+	// List of all TTIDs retrieved from this query
+	$ttids_by_pid = array_column($map_pid2ttid, 'term_taxonomy_id');
 
-			$term_id = $wpdb->get_var( $wpdb->prepare(
-				"SELECT term_id FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy = %s AND term_taxonomy_id = %d",
-				'product_cat', $ttid ) );
-error_log(print_r('term id', TRUE));
-error_log(print_r($term_id, TRUE));
-			if ($term_id === NULL) continue;
+	// 3) DB: Find all term_id's for these term_taxonomy_id's
+	
+	// Hit the DB again
+	$map_ttid2tid = $wpdb->get_results( $wpdb->prepare(
+		"SELECT term_taxonomy_id,term_id
+			FROM {$wpdb->prefix}term_taxonomy
+			WHERE taxonomy = %s
+				AND term_taxonomy_id IN (" . implode(',', $ttids_by_pid) . ")",
+		'product_cat' ), OBJECT_K );
 
-			$term_info = $wpdb->get_row( $wpdb->prepare(
-				"SELECT name, slug FROM {$wpdb->prefix}terms WHERE term_id = %d",
-				$term_id ) );
-error_log(print_r('term_info', TRUE));
-error_log(print_r($term_info, TRUE));
-			$categories[] = array(
-				'cat_id' => $term_id,
-				'name' => $term_info->name,
-				'slug' => $term_info->slug, );
+	// List of all TIDs from this query
+	$tids_by_ttid = array_map( function($x){return $x->term_id;}, $map_ttid2tid );
+
+	// 4) DB: Find all category information for these term_id's
+	
+	$map_tid2cat = $wpdb->get_results( $wpdb->prepare(
+		"SELECT term_id,name,slug
+			FROM {$wpdb->prefix}terms
+			WHERE term_id IN (" . implode(',', $tids_by_ttid) . ")",
+		), OBJECT_K );
+
+	// 5) Match it all up
+	
+	// Cycle through each line item
+	foreach( $response->data['line_items'] as $k => $lineitem ) {
+		
+		// Initialize the categories array
+		$response->data['line_items'][$k]['categories'] = [];
+		
+		foreach( $map_pid2ttid as $kk => $pidttid ) {
+
+			// Not the droids you're looking for
+			if ($pidttid['object_id'] != $lineitem['product_id'])
+				continue;
+			
+			// Not sure why this would happen but eh, it did so I'm checking now
+			if (!$pidttid['term_taxonomy_id'] || !$map_ttid2tid[ $pidttid['term_taxonomy_id'] ]->term_id)
+				continue;
+
+			// We have found a TTID that's associated with this PID
+			// Keep in mind that there may be multiple TTIDs for this PID
+			// but we can at least process this one.
+			
+			// No need to have to cycle through it again...
+			unset($map_pid2ttid[$kk]);
+			
+			// This TTID maps to exactly one TID, which itself maps to exactly one category
+			$response->data['line_items'][$k]['categories'][] = [
+				'id'	=>               $map_ttid2tid[ $pidttid['term_taxonomy_id'] ]->term_id         ,
+				'name'	=> $map_tid2cat[ $map_ttid2tid[ $pidttid['term_taxonomy_id'] ]->term_id ]->name ,
+				'slug'	=> $map_tid2cat[ $map_ttid2tid[ $pidttid['term_taxonomy_id'] ]->term_id ]->slug ,
+			];
 
 		}
-
-		$response->data['line_items'][$k]['categories'] = $categories;
-		
 	}
-
-
-
-
-
-//	error_log($object->get_id());
-//	$response->data['crex'] = 'diditwork';
+	
+	// We're done here, send it to the next filter
 	return $response;
+
 }
 
-add_filter( 'woocommerce_rest_prepare_shop_order_object', 'thisthingy', 10, 3 );
-
-
-
-
-
-/*
-
-$product_ids = "select product_id from wp_wc_order_product_lookup where order_id = {$object->get_id()};";
-
-foreach ($pid in $product_ids) {
-
-	$term_taxonomy_ids = "select term_taxonomy_id from wp_term_relationships where object_id = $pid;";
-	
-	$categories = {};
-
-	foreach($ttid in $term_taxonomy_ids): {
-
-		$term_id = "select * from wp_term_taxonomy where taxonomy = 'product_cat' and term_taxonomy_id = $ttid;";
-		if (!term_id) continue;
-
-		// This term ID can be thought of as a category ID now
-		// Get other category information......
-
-		$name, $slug = "select name, slug from wp_terms where term_id = $term_id;";
-
-		
-*/
+// Many thanks to:
+// woocommerce/packages/woocommerce-rest-api/src/Controllers/Version2/class-wc-rest-orders-v2-controller.php:322
+// for making this filter a thing.
+// Without it, this would be a lot harder to do.
+add_filter( 'woocommerce_rest_prepare_shop_order_object', 'add_categories', 10, 3 );
